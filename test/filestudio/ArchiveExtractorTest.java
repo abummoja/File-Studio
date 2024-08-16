5b6a0daf3377940de3459ffe33db987d90c72bd6
Abraham <abummoja3@gmail.com>
2024-08-16 10:52:48 +0300

    unit testing (rar,7z,multiformat fail)need libs

--- test/filestudio/ArchiveExtractorTest.java
+++ test/filestudio/ArchiveExtractorTest.java
@@ -23,9 +23,11 @@ import org.testng.annotations.Test;
  */
 public class ArchiveExtractorTest {
 
+    ArchiveExtractor ext = new ArchiveExtractor();
+
     @Test
     void TestCompressor() throws IOException {
-        ArchiveExtractor ext = new ArchiveExtractor();
+        //ext = new ArchiveExtractor();
         String outputPath = "C:\\Users\\Admin\\Downloads\\test.zip";
         String path = "C:\\Users\\Admin\\Downloads\\gnome";
         File outputZip = new File(outputPath);
@@ -36,11 +38,21 @@ public class ArchiveExtractorTest {
     }
 
     @Test
-    void TestArchiver() {
-        ArchiveExtractor ext = new ArchiveExtractor();
+    void TestUnArchiver() {
+        //ext = new ArchiveExtractor();
         String theFile, oDir;
         theFile = "C:\\Users\\Admin\\Downloads\\leetcode-solutions-master.zip";
         oDir = "C:\\Users\\Admin\\Downloads\\leetsoln";
         ext.unarchive(theFile, oDir);
+    }
+
+    @Test
+    void TestArchiver() {
+        String theDir, newFile;
+        theDir = "C:\\Users\\Admin\\Downloads\\ABRAHAM 32101";
+        //creating 7z fails (org.tukaani.xz lib required) also rar cannot be made from this.
+        //only single formats such as .tar, .zip (not .tar.gz or .tar.xz)
+        newFile = "C:\\Users\\Admin\\Downloads\\testarch.tar";
+        ext.archive(theDir, newFile);
     }
 }
